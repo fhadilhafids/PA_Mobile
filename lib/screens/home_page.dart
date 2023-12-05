270338778483ded84ad6c1c61f5e942037cf41b0
Maulana M Hafidz <maulanasmd123@gmail.com>
2023-12-05 20:04:13 +0800

    Perbaikan desain home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -150,6 +150,172 @@ class _HomePageState extends State<HomePage> {
       body: Center(
         child: ListView(children: [
           const Padding(
+            padding: EdgeInsets.only(left: 20, top: 15),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Offers & Deals",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              CarouselSlider(
+                items: imageSliders,
+                options: CarouselOptions(
+                    autoPlay: true,
+                    enlargeCenterPage: true,
+                    aspectRatio: 3.1,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _current = index;
+                      });
+                    }),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: imgList.map((url) {
+                  int index = imgList.indexOf(url);
+                  return Container(
+                    width: 8,
+                    height: 8,
+                    margin: EdgeInsets.symmetric(
+                      vertical: 0,
+                      horizontal: 3,
+                    ),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _current == index
+                          ? Theme.of(context).brightness == Brightness.light
+                              ? Color.fromRGBO(0, 0, 0, 0.9)
+                              : Color.fromRGBO(197, 197, 197, 0.898)
+                          : Theme.of(context).brightness == Brightness.light
+                              ? Color.fromRGBO(0, 0, 0, 0.4)
+                              : Color.fromRGBO(163, 163, 163, 0.894),
+                    ),
+                  );
+                }).toList(),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 20, top: 15),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Categories",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(children: <Widget>[
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const exploreCategory(
+                                      kategori: 'kitchen',
+                                    )));
+                      },
+                      child: Container(
+                        width: 80,
+                        height: 70,
+                        margin: const EdgeInsets.only(top: 10, left: 20),
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Icon(
+                          Icons.dining,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const exploreCategory(
+                                      kategori: 'electronic',
+                                    )));
+                      },
+                      child: Container(
+                        width: 80,
+                        height: 70,
+                        margin: const EdgeInsets.only(top: 10, left: 20),
+                        decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Icon(
+                          Icons.tv,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const exploreCategory(
+                                      kategori: 'tables & chairs',
+                                    )));
+                      },
+                      child: Container(
+                        width: 80,
+                        height: 70,
+                        margin: const EdgeInsets.only(top: 10, left: 20),
+                        decoration: BoxDecoration(
+                          color: Colors.cyan,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Icon(
+                          Icons.desk,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const exploreCategory(
+                                      kategori: 'bathroom',
+                                    )));
+                      },
+                      child: Container(
+                        width: 80,
+                        height: 70,
+                        margin: const EdgeInsets.only(top: 10, left: 20),
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Icon(
+                          Icons.bathroom,
+                          size: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ])),
+            ],
+          ),
+          const Padding(
             padding: EdgeInsets.only(left: 20, top: 15, bottom: 15),
             child: Align(
               alignment: Alignment.centerLeft,
@@ -226,172 +392,6 @@ class _HomePageState extends State<HomePage> {
                 );
               },
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 20, top: 15),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Categories",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(children: <Widget>[
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const exploreCategory(
-                                  kategori: 'kitchen',
-                                )));
-                  },
-                  child: Container(
-                    width: 80,
-                    height: 70,
-                    margin: const EdgeInsets.only(top: 10, left: 20),
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Icon(
-                      Icons.dining,
-                      size: 30,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const exploreCategory(
-                                  kategori: 'electronic',
-                                )));
-                  },
-                  child: Container(
-                    width: 80,
-                    height: 70,
-                    margin: const EdgeInsets.only(top: 10, left: 20),
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Icon(
-                      Icons.tv,
-                      size: 30,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const exploreCategory(
-                                  kategori: 'tables & chairs',
-                                )));
-                  },
-                  child: Container(
-                    width: 80,
-                    height: 70,
-                    margin: const EdgeInsets.only(top: 10, left: 20),
-                    decoration: BoxDecoration(
-                      color: Colors.cyan,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Icon(
-                      Icons.desk,
-                      size: 30,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const exploreCategory(
-                                  kategori: 'bathroom',
-                                )));
-                  },
-                  child: Container(
-                    width: 80,
-                    height: 70,
-                    margin: const EdgeInsets.only(top: 10, left: 20),
-                    decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Icon(
-                      Icons.bathroom,
-                      size: 20,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ])),
-          const Padding(
-            padding: EdgeInsets.only(left: 20, top: 15),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Offers & Deals",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          Column(
-            children: [
-              CarouselSlider(
-                items: imageSliders,
-                options: CarouselOptions(
-                    autoPlay: true,
-                    enlargeCenterPage: true,
-                    aspectRatio: 3.1,
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        _current = index;
-                      });
-                    }),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: imgList.map((url) {
-                  int index = imgList.indexOf(url);
-                  return Container(
-                    width: 8,
-                    height: 8,
-                    margin: EdgeInsets.symmetric(
-                      vertical: 0,
-                      horizontal: 3,
-                    ),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: _current == index
-                          ? Theme.of(context).brightness == Brightness.light
-                              ? Color.fromRGBO(0, 0, 0, 0.9)
-                              : Color.fromRGBO(197, 197, 197, 0.898)
-                          : Theme.of(context).brightness == Brightness.light
-                              ? Color.fromRGBO(0, 0, 0, 0.4)
-                              : Color.fromRGBO(163, 163, 163, 0.894),
-                    ),
-                  );
-                }).toList(),
-              )
-            ],
           ),
         ]),
       ),
